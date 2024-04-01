477b750266ab5d84189247dee39ce8cdaf778857
strateg213 <strateg213@gmail.com>
2024-04-01 19:35:52 +0300

    feat: update task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -33,7 +33,8 @@ function getCurrentFunctionName() {
  *
  */
 function getFunctionBody(func) {
-  return `${func()}`;
+  const str = func.toString();
+  return str;
 }
 
 /**
@@ -50,8 +51,8 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.length;
 }
 
 /**
